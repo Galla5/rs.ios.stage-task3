e5787d11bd535d64a4f90edf4b96a5e7f16a7a4e
Poliakowa Galina <galapoliakowa2015@gmail.com>
2021-05-23 17:42:18 +0300

    feat: add solutions for task1, task3

--- Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
@@ -5,25 +5,64 @@
 #pragma mark - First
 
 -(NSString *)monthNameBy:(NSUInteger)monthNumber {
+    if(monthNumber == 1) {
+        return @"January";
+    } else if(monthNumber == 2) {
+        return @"February";
+    } else if(monthNumber == 3) {
+        return @"March";
+    } else if(monthNumber == 4) {
+        return @"April";
+    } else if(monthNumber == 5) {
+        return @"May";
+    } else if(monthNumber == 6) {
+        return @"June";
+    } else if(monthNumber == 7) {
+        return @"Jule";
+    } else if(monthNumber == 8) {
+        return @"August";
+    } else if(monthNumber == 9) {
+        return @"September";
+    } else if(monthNumber == 10) {
+        return @"October";
+    } else if(monthNumber == 11) {
+        return @"November";
+    } else if(monthNumber == 12) {
+        return @"December";
+    }
     return nil;
 }
 
 #pragma mark - Second
 
 - (long)dayFromDate:(NSString *)date {
-    return 0;
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    formatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ss'Z'";
+    NSDate *dateFromString = [formatter dateFromString:date];
+    NSDateFormatter *dayFormatter = [[NSDateFormatter alloc] init];
+    dayFormatter.dateFormat = @"dd";
+    return [dayFormatter stringFromDate: dateFromString].intValue;
 }
 
 #pragma mark - Third
 
 - (NSString *)getDayName:(NSDate*) date {
-    return nil;
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    formatter.locale = [[NSLocale alloc]initWithLocaleIdentifier:@"ru_RU"];
+    formatter.dateFormat = @"EEEEEE";
+    return [formatter stringFromDate: date];
 }
 
 #pragma mark - Fourth
 
 - (BOOL)isDateInThisWeek:(NSDate *)date {
-    return NO;
+    NSCalendar *gregorian = [[NSCalendar alloc]
+                             initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    NSDateComponents *todaysComponents = [gregorian components:NSCalendarUnitWeekOfYear fromDate:[NSDate date]];
+    NSUInteger todaysWeek = [todaysComponents weekOfYear];
+    NSDateComponents *otherComponents = [gregorian components:NSCalendarUnitWeekOfYear fromDate:date];
+    NSUInteger datesWeek = [otherComponents weekOfYear];
+    return (todaysWeek==datesWeek);
 }
 
 @end
--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -9,7 +9,30 @@
 @implementation LexicographicallyMinimal
 
 -(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
-    return nil;
+    NSMutableArray *resultChars = [[NSMutableArray alloc] initWithCapacity:[string1 length] + [string2 length]];
+    
+    NSMutableArray *firstChars = [[NSMutableArray alloc] initWithCapacity:[string1 length]];
+    NSMutableArray *secondChars = [[NSMutableArray alloc] initWithCapacity:[string2 length]];
+
+    int maxCount = [string1 length] > [string2 length] ? [string1 length] : [string2 length];
+    
+    for (int i=0; i < maxCount; i++) {
+        if (i < [string1 length]) {
+            NSString *ichar  = [NSString stringWithFormat:@"%c", [string1 characterAtIndex:i]];
+            [resultChars addObject: ichar];
+        }
+        
+        if (i < [string2 length]) {
+            NSString *ichar  = [NSString stringWithFormat:@"%c", [string2 characterAtIndex:i]];
+            [resultChars addObject: ichar];
+        }
+    }
+    
+    NSMutableString *resultString = [NSMutableString stringWithCapacity:[string1 length] + [string2 length]];
+    for(id resutlString in resultChars) {
+        [resultString appendString: resutlString];
+    }
+    return  [NSString stringWithString:resultString];
 }
 
 @end
